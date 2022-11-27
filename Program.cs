df49b12a40d38d9113e3cbf7952ecdea7311632e
masyaka.94 <masyaka.94@gmail.com>
2022-11-27 11:57:26 +0500

    Исправлены ошибки в коде, применено форматирование

--- Program.cs
+++ Program.cs
@@ -1,58 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string [] GetTextFromUser (int length)
+string[] GetTextFromUser(int length)
 {
-string[] array = new string [length];
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write($"Введите {i+1} элемент массива: ");
-    array[i] = (Console.ReadLine()??"0");
-}
-return array;
+    string[] array = new string[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = (Console.ReadLine() ?? "0");
+    }
+    return array;
 }
 
 int GetNewArrayLength(string[] TextArray)
 {
-int count = 0;
-for (int i = 0; i < TextArray.Length; i++)
-{
-    if(TextArray[i].Length<=3)
+    int count = 0;
+    for (int i = 0; i < TextArray.Length; i++)
     {
-        count++;
+        if (TextArray[i].Length <= 3)
+        {
+            count++;
+        }
     }
-}
-return count;
+    return count;
 }
 
-string [] CreateArrayOfShortString (string[] TextArray, int count)
+string[] CreateArrayOfShortString(string[] TextArray, int count)
 {
-int newcount = 0;
-string[] ArrayofShortString = new string [count];
-for (int i = 0; i < TextArray.Length; i++)
-{
-    if(TextArray[i].Length<=3)
+    int newcount = 0;
+    string[] ArrayofShortString = new string[count];
+    for (int i = 0; i < TextArray.Length; i++)
     {
-        ArrayofShortString[newcount] = TextArray[i];
-        newcount++;
+        if (TextArray[i].Length <= 3)
+        {
+            ArrayofShortString[newcount] = TextArray[i];
+            newcount++;
+        }
     }
-}
-return ArrayofShortString;
+    return ArrayofShortString;
 }
 
 void PrintArray(string[] TextArray)
 {
-foreach (var item in TextArray)
-{
-    Console.Write(item + " ");
-}
+    foreach (var item in TextArray)
+    {
+        Console.Write(item + " ");
+    }
 }
 Console.Write("Введите количество элементов:\t ");
-int arrayLenght = int.Parse(Console.ReadLine()??"0");
-string[] ArrayofString = GetTextFromUser(arrayLenght);
-int newArrayLength = GetNewArrayLength(ArrayofString);
-string [] newArray = CreateArrayOfShortString(TextArray: ArrayofString,count: newArrayLength);
+int N = int.Parse(Console.ReadLine() ?? "0");
+string[] ArrayofString = GetTextFromUser(N);
+int M = GetNewArrayLength(ArrayofString);
+string[] newArray = CreateArrayOfShortString(TextArray: ArrayofString, count: M);
 
-Console.Write("Получился вот такой массив строк: ");
+Console.Write("Получился массив строк: ");
 PrintArray(ArrayofString);
 Console.WriteLine();
 Console.WriteLine("Массив строк с количеством элементов <=3: ");
